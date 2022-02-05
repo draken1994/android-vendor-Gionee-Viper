7d948e5ea6e2d7faec76f938dee571085d9408dd
draken1994 <dwkryadav25@gmail.com>
2022-02-05 08:59:37 +0530

    draken

--- vendor.mk
+++ vendor.mk
@@ -1,11 +1,11 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Viper/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Gionee/Elife/proprietary/vendor,system/vendor)
